860a3616fbeaa1a4be85e0e9f10ce78eafce8cf1
Apryms <madonaapry@gmail.com>
2022-07-25 22:14:55 +0700

    finish export and print hasil kegiatan

--- resources/views/pages/pupr/bina-marga/index-hasil.blade.php
+++ resources/views/pages/pupr/bina-marga/index-hasil.blade.php
@@ -2,6 +2,19 @@
 @section('title')
     PUPR
 @endsection
+@push('addon-script')
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.12.1/b-2.2.3/b-colvis-2.2.3/b-html5-2.2.3/fc-4.1.0/datatables.min.js"></script>
+
+@endpush
 @section('content')
 <div id="content" class="flex ">
     @include('alert.success')
@@ -59,8 +72,29 @@
 @endsection
 
 @push('addon-script')
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+
 <script>
+    
     var datatable = $('#datatable').DataTable({
+            dom: 'lBfrtip',
+            buttons: [
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2,3,4, 5,6 ]
+                }
+            },
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: [ 0, 1, 2,3,4, 5,6 ]
+                }
+            },
+        ],
+            // buttons: [
+            //         'excel', 'pdf', 'print'
+            //     ],
             processing: true,
             serverSide:true,
             ordering:true,
@@ -84,5 +118,7 @@
                 },
             ]
         });
+        
 </script>
+
 @endpush
